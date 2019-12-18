907308deb86262e7369d6f11f0484ebd5d7c274b
zacisgr8 <43868294+zacisgr8@users.noreply.github.com>
2019-12-18 09:02:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.urandom(32) or "zack-Ali"
+    SECRET_KEY = os.urandom(32) 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'zack.db')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
