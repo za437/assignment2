5b0f3cc01cd87f7ef19d4f21346141aaa2e371e4
zacisgr8 <43868294+zacisgr8@users.noreply.github.com>
2019-12-18 04:35:07 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from config import Config
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
